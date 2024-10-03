1eb72faf262451aa315d12b83303ec169756aee6
Venkatesh <venkateshpk06@gmail.com>
2024-10-03 22:34:01 +0530

    commit

--- userController.js
+++ userController.js
@@ -40,28 +40,29 @@ const UserController = {
             if (!user) {
                 return res.status(401).json({ message: "Invalid email or password" });
             }
-
+    
             const isMatch = await bcrypt.compare(password, user.password);
             if (!isMatch) {
                 return res.status(401).json({ message: "Invalid email or password" });
             }
-
+    
             const token = jwt.sign({ id: user._id }, config.JWT_secret, { expiresIn: "24h" });
-            const isProduction = process.env.NODE_ENV === 'production';
-
+    
             res.cookie("token", token, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',  // Ensure this works in development
+                secure: process.env.NODE_ENV === 'production',
                 sameSite: process.env.NODE_ENV === 'production' ? "none" : "lax",
                 maxAge: 24 * 60 * 60 * 1000  // 24 hours
             });
-
-            res.status(200).json({ message: "User logged in successfully", token, user: { id: user._id, name: user.userName } });
+    
+            res.status(200).json({ 
+                message: "User logged in successfully", 
+                user: { id: user._id, name: user.userName }
+            });
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
     },
-
     //get the current user
     Me: async (req, res) => {
         try {
