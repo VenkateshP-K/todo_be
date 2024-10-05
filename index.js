b42cafbe1b05b5751b038aca82b642b3144a2750
Venkatesh <venkateshpk06@gmail.com>
2024-10-05 22:25:32 +0530

    commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(cookieParser());
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://hotelbooking-demo.netlify.app', 'https://hotel-booking-be-6h4d.onrender.com'],
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
